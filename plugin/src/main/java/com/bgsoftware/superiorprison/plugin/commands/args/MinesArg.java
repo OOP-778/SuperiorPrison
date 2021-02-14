83961efd2bb1e537fbd3ed4879d98a67213078fa
oop-778 <oskardhavel@gmail.com>
2021-02-14 11:26:12 +0200

    Now you can reset mines like /mines reset all

--- plugin/src/main/java/com/bgsoftware/superiorprison/plugin/commands/args/MinesArg.java
+++ plugin/src/main/java/com/bgsoftware/superiorprison/plugin/commands/args/MinesArg.java
@@ -6,30 +6,39 @@ import com.bgsoftware.superiorprison.plugin.object.mine.SNormalMine;
 import com.oop.orangeengine.command.OCommand;
 import com.oop.orangeengine.command.arg.CommandArgument;
 import com.oop.orangeengine.main.util.data.pair.OPair;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public class MinesArg extends CommandArgument<SNormalMine> {
-  public MinesArg() {
-    setDescription("A mine");
-    setIdentity("mine");
-    setMapper(
-        mineName -> {
-          Optional<SuperiorMine> superiorMine =
-              SuperiorPrisonPlugin.getInstance().getMineController().getMines().stream()
-                  .filter(mine -> mine.getName().equalsIgnoreCase(mineName))
-                  .findFirst();
-          return new OPair<>(
-              (SNormalMine) superiorMine.orElse(null), "Failed to find mine with name " + mineName);
-        });
-  }
+public class MinesArg extends CommandArgument<Collection<SuperiorMine>> {
+    public MinesArg() {
+        setDescription("Mine target");
+        setIdentity("mines");
+        setMapper(
+            mineName -> {
+                if (mineName.equalsIgnoreCase("all"))
+                    return new OPair<>(SuperiorPrisonPlugin.getInstance().getMineController().getMines(), null);
+                else {
+                    List<SuperiorMine> collect = SuperiorPrisonPlugin.getInstance()
+                        .getMineController().getMines().stream()
+                        .filter(mine -> mine.getName().contains(mineName))
+                        .collect(Collectors.toList());
+                    return new OPair<>(collect.isEmpty() ? null : collect, "Failed to find mines matching: " + mineName);
+                }
+            });
+    }
 
-  @Override
-  public void onAdd(OCommand command) {
-    command.nextTabComplete(
-        (previous, args) ->
-            SuperiorPrisonPlugin.getInstance().getMineController().getMines().stream()
-                .map(SuperiorMine::getName)
-                .collect(Collectors.toList()));
-  }
+    @Override
+    public void onAdd(OCommand command) {
+        command.nextTabComplete(
+            (previous, args) -> {
+                List<String> collect = SuperiorPrisonPlugin.getInstance().getMineController()
+                    .getMines().stream()
+                    .map(SuperiorMine::getName)
+                    .collect(Collectors.toList());
+                collect.add("all");
+                return collect;
+            });
+    }
 }
